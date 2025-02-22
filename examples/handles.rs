9301b124c523a2cae6c68c529e2b0e22591c5be4
Isaac Holston <32341824+iholston@users.noreply.github.com>
2025-02-22 11:42:54 -0500

    refactor: add pause_handle example

--- examples/handles.rs
+++ examples/handles.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 use win_hotkeys::HotkeyManager;
 use win_hotkeys::VKey;
 
-fn main() {
+fn main() { 
     // Create the manager
     let mut hkm = HotkeyManager::new();
 
@@ -14,12 +14,25 @@ fn main() {
     .unwrap();
 
     // Get an interrupt handle that can be used to interrupt / stop the event loop from any thread
-    let handle = hkm.interrupt_handle();
+    let interrupt_handle = hkm.interrupt_handle();
 
-    // Create a second thread that will stop the event loop after 5 seconds
+    // Get a pause handle that can be used to programmatically pause the processing of hotkeys from
+    // any thread. Note: registered pause hotkeys will still be processed.
+    let pause_handle = hkm.pause_handle();
+
+    // Create a second thread that will pause/interrupt hkm
     spawn(move || {
-        sleep(Duration::from_secs(5));
-        handle.interrupt();
+        sleep(Duration::from_secs(3));
+        
+        println!("Pausing hotkeys for 3 seconds");
+        pause_handle.toggle();
+        sleep(Duration::from_secs(3));
+
+        println!("Unpausing hotkeys");
+        pause_handle.toggle();
+
+        sleep(Duration::from_secs(3));
+        interrupt_handle.interrupt();
     });
 
     // Run the event handler in a blocking loop. This will block until interrupted and execute the
